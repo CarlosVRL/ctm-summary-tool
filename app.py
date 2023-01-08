b20e391529932965d025534033c6bb689b887104
carlosvrl <carlos.varela.eng@gmail.com>
2023-01-07 22:48:05 -0600

    :bookmark: Initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+from src.example import hello
+
+
+if __name__ == "__main__":
+    print(hello())
---
+++ src/example.py
@@ -0,0 +1,9 @@
+from src import ROOT_PATH
+
+
+def hello():
+    return "Hello World!"
+
+
+def root_path():
+    return ROOT_PATH
